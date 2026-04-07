bf1b73b03f9966bf6c650245901bafa1bbb20bdf
c2065535 <d.leivdal2@newcastle.ac.uk>
2026-04-07 16:29:35 +0100

    Start

---
+++ contact.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en-GB">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home - My Website</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div>
+        <p>Hello, World!</p>
+        <p>This is the contact page.</p>
+    </div>
+    <nav>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="about.php">About</a></li>
+        <li><a href="contact.php">Contact</a></li>
+    </ul>
+</nav>
+</body>
+</html>
---
+++ planning/checklist.md
@@ -0,0 +1,13 @@
+# Session Checklist
+
+## Start of Session
+- [ ] Copy project from Home Drive (H:) to C:\wamp64\www\
+- [ ] Start WAMP (check for green icon)
+- [ ] Test site loads at http://localhost/set11112/
+- [ ] Import database
+
+## End of Session
+- [ ] Export database to .sql file 
+- [ ] Save progress to git
+- [ ] Verify backup is complete
+- [ ] Close WAMP
