708fc44aef0c14d7621dda412ceb29af9199177f
Mollusk-jpg <d.leivdal2@newcastle.ac.uk>
2026-04-07 18:53:30 +0100

    Add observations page

--- contact.php
+++ contact.php
@@ -1,21 +1,61 @@
 <?php
+/**
+ * Scottish Mammal Observations Database - Home Page
+ * Displays a list of all Scottish mammal species
+ *
+ * SET08101 Web Technologies Coursework Starter Code
+ */
+
+require_once 'includes/db.php';
+
+$pageTitle = 'All Observations';
+
+// Fetch all species from the database
+$pdo = getDbConnection();
+$stmt = $pdo->query('
+    SELECT
+        id,
+        locality,
+        individual_count,
+        latitude,
+        longitude,
+        gbif_species_key,
+        observation_date
+    FROM observations
+    ORDER BY id
+');
+$observations = $stmt->fetchAll();
 
 require_once 'includes/header.php';
-
 ?>
 
-<!DOCTYPE html>
-<html lang="en-GB">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home - My Website</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-    <div>
-        <p>Hello, World!</p>
-        <p>This is the contact page.</p>
-    </div>
-</body>
-</html>
+
+<?php if (empty($observations)): ?>
+    <p>No observations found in the database.</p>
+<?php else: ?>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Individual Count</th>
+                <th>Latitude</th>
+                <th>Longitude</th>
+                <th>Observation Date</th>
+                <th>Species Key</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($observations as $ob): ?>
+                <tr>
+                    <td><?php echo e($ob['id']); ?></td>
+                    <td><?php echo e($ob['individual_count']); ?></td>
+                    <td><?php echo e($ob['latitude']); ?></td>
+                    <td><?php echo e($ob['longitude']); ?></td>
+                    <td><?php echo e($ob['observation_date']); ?></td>
+                    <td><?php echo e($ob['gbif_species_key']); ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+<?php endif; ?>
+
