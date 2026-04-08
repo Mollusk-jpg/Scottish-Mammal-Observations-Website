6161882a49c3943249685ea56fb51168ed83c049
Mollusk-jpg <d.leivdal2@newcastle.ac.uk>
2026-04-08 18:09:22 +0100

    Update contact.php > observations.php

--- includes/header.php
+++ includes/header.php
@@ -13,7 +13,7 @@
         <nav>
             <a href="index.php">Home</a>
             <a href="about.php">About</a>
-            <a href="contact.php">Contact</a>
+            <a href="observations.php">Observations</a>
 
             <!-- Students: Add more navigation links here -->
         </nav>
