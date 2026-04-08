6a0adb9ce644d18d65b3e74c87186271dbf857be
Mollusk-jpg <d.leivdal2@newcastle.ac.uk>
2026-04-08 18:43:06 +0100

    Add list of animal names in about.php

--- about.php
+++ about.php
@@ -1,21 +1,47 @@
 <?php
 
-require_once 'includes/header.php';
 
+require_once 'includes/config.php';
+require_once 'includes/db.php';
+
+
+$pageTitle = 'All Observations';
+
+$pdo = getDbConnection();
+$stmt = $pdo->query('
+    SELECT
+        common_name,
+        species_name
+    FROM species
+    ORDER BY common_name
+');
+$species_names = $stmt->fetchAll();
+
+require_once 'includes/header.php';
 ?>
 
-<!DOCTYPE html>
-<html lang="en-GB">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home - My Website</title>
-    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+
+<?php if (empty($species_names)): ?>
+    <p>No species names found in the database.</p>
+<?php else: ?>
+<table>
+    <thead>
+        <tr>
+            <th>Common Name</th>
+        </tr>
+    </thead>
     <div>
         <p>Hello, World!</p>
-        <p>This is the about page</p>
+        <!-- $names = '' -->
+        <?php   foreach ($species_names as $names): ?>
+                    <p><?php echo e($names['common_name']); ?></p>
+        <?php   endforeach;?>
+        <a href="index.php">Home</a>
     </div>
+<table>
+<?php endif; ?>
+
 </body>
 </html>
