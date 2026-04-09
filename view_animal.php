c0b16f2c8f71979b73c15ae19d2b847081850b50
Mollusk-jpg <d.leivdal2@newcastle.ac.uk>
2026-04-09 18:51:40 +0100

    Add 'back to species list' link

--- view_animal.php
+++ view_animal.php
@@ -11,7 +11,6 @@ if (!isset($_GET['key']) || !is_numeric($_GET['key'])) {
 
 $speciesKey = (int)$_GET['key'];
 
-// Fetch the specific id and (try to) merge wiith category name
 $pdo = getDbConnection();
 $stmt = $pdo->query('
     SELECT
@@ -26,20 +25,39 @@ $species_names = $stmt->fetchAll();
 $observations = getAllObservations();
 $species = getAllSpecies();
 
+
 $page_name = '';
 
 $coordinate_x = 0;
 $coordinate_y = 0;
 
 $list = array();
+
+// Create list of latitude and longitude points. (currently returns string, will fix later)
+foreach ($species as $specie): 
+    if ($specie['gbif_species_key'] == $speciesKey){
+        echo $specie['common_name'], " Latitude and Longitude: "; 
+    } 
+    
+    foreach ($observations as $observation): 
+        if ($observation['gbif_species_key'] == $speciesKey){
+            $coordinate_x = $observation['latitude'];
+            $coordinate_y = $observation['longitude'];
+            if (! in_array($coordinate_x, $list) && ! in_array($coordinate_y, $list)){
+                $list[] = [$coordinate_x, $coordinate_y];
+            }
+        }
+    endforeach;
+endforeach;
 ?>
 
 
 <head>
 </head>
 <body>
+<p><a href="about.php">&larr; Back to species list</a></p>
 
-<!-- This is to make the header as the same animal clicked on in about.php -->
+<!-- This is to make the header the same animal clicked on in about.php -->
 <?php foreach ($species_names as $name):
 if ($name['gbif_species_key'] == $speciesKey){
     $page_name = $name['common_name'];
@@ -47,25 +65,9 @@ if ($name['gbif_species_key'] == $speciesKey){
 endforeach
 ?>
     <h1><?= $page_name ?></h1>
-    <ul>
-        <?php foreach ($species as $specie): 
-            if ($specie['gbif_species_key'] == $speciesKey){
-                echo $specie['common_name'], "8888"; 
-            } ?>
-            
-            <?php foreach ($observations as $observation): 
-                if ($observation['gbif_species_key'] == $speciesKey){
-                    $coordinate_x = $observation['latitude'];
-                    $coordinate_y = $observation['longitude'];
-                    if (! in_array($coordinate_x, $list) && ! in_array($coordinate_y, $list)){
-                        $list[] = [$coordinate_x, $coordinate_y];
-                        
-                    }
-                }
-            ?>
-            <?php endforeach; ?>
-            
-        <?php endforeach; ?>
-        <p><?= var_dump($list) ?></p>
-    </ul>
+    
+        <?php 
+        var_dump($list)
+        ?>
+    
 </body>
